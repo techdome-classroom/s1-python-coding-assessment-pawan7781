5aed68a4385d65751a6b667a5f686a57f8a85fbc
pawan7781 <155439048+pawan7781@users.noreply.github.com>
2024-09-27 12:38:08 +0530

    Update program1.py
    initial commit

--- program1.py
+++ program1.py
@@ -2,5 +2,29 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
+        # base case 
+        if self.is_inbounds(grid, row, col):
+	     # transform land into water so we don't revisit this cell.
+            grid[row][col] = "L"
+ 
+            for new_row, new_col in [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]:
+                self.dfs(new_row, new_col, grid)
+ 
+   # validation: row and col are within the grid and the cell is a land.
+    def is_inbounds(self, grid: list[list[str]], row: int, col: int) -> bool:
+        return row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0]) and grid[row][col] == "W"
+    
+    def numIslands(self, grid: list[list[str]]) -> int:
+        count = 0
+        num_rows = len(grid)
+        num_cols = len(grid[0])
+        
+        for row in range(0, num_rows):
+            for col in range(0, num_cols):
+                if grid[row][col] == "W":
+                    count += 1
+                    self.dfs(row, col, grid)
                     
-        return 0
+        print (count)
+    
+
